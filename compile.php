016d1795c482e585b2b28512b4719abeab0cf81a
Don Myers <myersd@trueaction.com>
2013-04-30 11:23:46 -0400

    more clean up

--- compile.php
+++ compile.php
@@ -15,39 +15,45 @@ $filename = basename(__FILE__);
 
 date_default_timezone_set('America/New_York');
 
-define('APC_CLEAR_CACHE', true);
-define('APC_COMPILE_RECURSIVELY', true);
-define('APC_COMPILE_FILE','/Users/myersd/Desktop/');
+$show_caches = true;
 
-define('APC_COMPILE_DIR','/Applications/MAMP/htdocs/expressionengine');
+/* compile this */
+$apc_compile_dir = '/Applications/MAMP/htdocs/basicmvc-template';
+
+/* desktop */
+$apc_compile_file = '/Users/myersd/Desktop/';
 
 echo 'APC Directory Compiler '.gmdate('Y-m-d H:i:s').chr(10);
+echo 'Compile '.$apc_compile_dir.chr(10);
 
-if (APC_CLEAR_CACHE){
-	echo (apc_clear_cache() ? 'Cache Cleaned' : 'Cache Not Cleaned').chr(10);
-	//var_dump(apc_cache_info());
+echo (apc_clear_cache() ? 'Cache Cleaned' : 'Cache Not Cleaned').chr(10);
+
+if ($show_caches) {
+	var_dump(apc_cache_info());
 }
 
 echo 'Compile'.chr(10);
-echo (apc_compile_dir(APC_COMPILE_DIR, APC_COMPILE_RECURSIVELY) ? 'Cache Created' : 'Cache Not Created').chr(10);
+echo (apc_compile_dir($apc_compile_dir, true) ? 'Cache Created' : 'Cache Not Created').chr(10);
 
-//var_dump(apc_cache_info());
+if ($show_caches) {
+	var_dump(apc_cache_info());
+}
 
-echo apc_bin_dumpfile(null,null,APC_COMPILE_FILE.basename(APC_COMPILE_DIR).'.apc'); 
+$bytes = apc_bin_dumpfile(null,null,$apc_compile_file.basename($apc_compile_dir).'.apc'); 
+
+echo ($bytes) ? $bytes.'b' : 'Error';
 
 function apc_compile_dir($root, $recursively = true){
 	$compiled = true;
 
-	switch($recursively){
-		case true:
-			foreach(glob($root.'/*', GLOB_ONLYDIR) as $dir) {
-				$compiled = $compiled && apc_compile_dir($dir, $recursively);
-			}
-		case false:
-			foreach(glob($root.'/*.php') as $file) {
-				$compiled = $compiled && apc_compile_file($file);
-			}
-		break;
+	if ($recursively) {
+		foreach(glob($root.'/*', GLOB_ONLYDIR) as $dir) {
+			$compiled = $compiled && apc_compile_dir($dir, $recursively);
+		}
+	}
+
+	foreach(glob($root.'/*.php') as $file) {
+		$compiled = $compiled && apc_compile_file($file);
 	}
 
 	return  $compiled;
